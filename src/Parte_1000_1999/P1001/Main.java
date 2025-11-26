41ef4b90c261c67e630167f864b73073fa6aeb7c
danilo.caxias <dscr9669@gmail.com>
2025-11-26 13:35:58 -0300

    Atualização!

---
+++ src/Parte_1000_1999/P1001/Main.java
@@ -0,0 +1,16 @@
+package Parte_1000_1999.P1001;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner leitor = new Scanner(System.in);
+
+        int A = leitor.nextInt();
+        int B = leitor.nextInt();
+
+        System.out.println("X = " + (A+B));
+        leitor.close();
+    }
+}
+
